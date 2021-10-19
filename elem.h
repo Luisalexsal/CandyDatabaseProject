579d39aa9c785cb677d4558ca5ebdd56163ad946
Luisalexsal <luisalexsal@gmail.com>
2021-10-19 12:07:09 -0700

    Update elem.h

--- elem.h
+++ elem.h
@@ -1,4 +1,4 @@
-// CS311 Yoshii - el_t for HW8 Hash Table
+// CS311 Alex Salmeron
 // Element type of a list node is defined here
 // el_t can be changed by the client to fit its needs
 //-----------------------------------------------------
